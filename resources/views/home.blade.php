bafe707887436676dd87625d1c7de01ad8c51b8f
dekito278 <aderizki68@gmail.com>
2020-05-03 21:55:25 +0700

    skkrta

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,10 +4,10 @@
 <div class="container">
     <div class="row">
         <div class="col-md-6">
-            <create-post />
+            <Buat-pameran />
         </div>
         <div class="col-md-6 posts-container" style="height: 35rem; overflow-y: scroll">
-            <all-posts />
+            <Lihat-pameran />
         </div>
     </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::group(['middleware' => 'auth', 'prefix' => 'post'], function () {
-    Route::get('get_all', 'PostController@getAllPosts')->name('fetch_all');
-    Route::post('create_post', 'PostController@createPost')->name('create_post');
+    Route::get('get_all', 'PameranController@AmbilDataPameran')->name('fetch_all');
+    Route::post('MembuatPameran', 'PameranController@createPost')->name('MembuatPameran');
 });
 
 Route::get('/home', 'HomeController@index')->name('home');
